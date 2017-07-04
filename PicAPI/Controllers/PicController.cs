bbb069fb7ee054bec8061542035992c1127dd762
Kieran Sisam <kieransisam@gmail.com>
2017-07-04 15:13:12 +1200

    Add project files.

---
+++ PicAPI/Controllers/PicController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace PicAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class PicController : Controller
+    {
+        private usapics_dbContext _db;
+
+        public PicController(usapics_dbContext db)
+        {
+            _db = db;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Its", "Working" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{swlat}/{swlong}/{nelat}/{nelong}")]
+        public ActionResult Get(double swlat, double swlong, double nelat, double nelong)
+        {
+          
+         var b = from p in _db.MetaImages
+                 where p.lat > swlat &&
+                       p.lng > swlong &&
+                       p.lat < nelat &&
+                       p.lng < nelong
+                       select p;
+
+             return Ok(b.ToList());
+        }
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PicAPI/Models/usapics_dbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PicAPI
+{
+    public class usapics_dbContext: DbContext
+    {
+        public usapics_dbContext(DbContextOptions<usapics_dbContext> options)
+           : base(options)
+        { }
+
+        public DbSet<MetaImages> MetaImages { get; set; }
+    }
+
+    public class MetaImages
+    {
+        public string path { get; set; }
+        public string id { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+        public double lat { get; set; }
+        public double lng { get; set; }
+        public DateTime taken { get; set; }
+        public string bucketname { get; set; }
+        public string urlo { get; set; }
+        public string urls { get; set; }
+        public string urlm { get; set; }
+        public string urll { get; set; }
+        public string urlt { get; set; }
+        public string urln { get; set; }
+        public string urlx { get; set; }
+
+    }
+}
